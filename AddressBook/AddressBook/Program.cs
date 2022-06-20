02abab6cb6e92e097df3780dedd19dccff921ee5
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-06-20 22:44:51 +0530

    Sorting contact by state in address book

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -10,7 +10,8 @@ while (true)
 {
     Console.WriteLine("Enter the number :\n1.Create Contact\n2.Update Contact\n3.Delete contact\n4.Add multiple Contact" +
                         "\n5.Add Unique Contact\n6.Display unique contact\n7.Search by city or state\n8.Count by city or state" +
-                        "\n9.Contact by city\n10.contact by state\n11.Sort contact by Name\n12.Sort contact by City\n13.View Contact");
+                        "\n9.Contact by city\n10.contact by state\n11.Sort contact by Name\n12.Sort contact by City\n13.Sort contact by State" +
+                        "\n14.View Contact");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -55,6 +56,9 @@ while (true)
             contact.SortContactByCity();
             break;
         case 13:
+            contact.SortContactByState();
+            break;
+        case14: 
             contact.ViewContact();
             break;
 
