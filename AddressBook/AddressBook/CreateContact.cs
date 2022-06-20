8a08fc3c8c775361c56ca2da8f634cac025c58b4
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-06-20 11:59:45 +0530

    Add new contact

---
+++ AddressBook/AddressBook/CreateContact.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class CreateContact
+    {
+        public List<Contacts> People = new List<Contacts>();
+        Dictionary<string, List<Contacts>> dict = new Dictionary<string, List<Contacts>>();
+
+        public void AddContact()
+        {
+            int flag = 0;
+            Contacts contact = new Contacts();
+            Console.WriteLine("Enter first Name");
+            string AddFirstName = contact.FirstName = Console.ReadLine();
+
+            foreach (var data in People)
+            {
+                if (People.Exists(data => data.FirstName == AddFirstName))
+                {
+                    flag++;
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("{0} already Exist! Can't take the Duplicate Record", AddFirstName);
+                    Console.ResetColor();
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("Enter Last Name : ");
+                contact.LastName = Console.ReadLine();
+
+                Console.WriteLine("Enter Email : ");
+                contact.Email = Console.ReadLine();
+
+                Console.WriteLine("Enter Phone Number : ");
+                contact.PhoneNumber = Console.ReadLine();
+
+                Console.WriteLine("Enter Address : ");
+                contact.Address = Console.ReadLine();
+
+                Console.WriteLine("Enter City : ");
+                contact.City = Console.ReadLine();
+
+                Console.WriteLine("Enter Zip : ");
+                contact.Zip = Console.ReadLine();
+
+                Console.WriteLine("Enter State : ");
+                contact.State = Console.ReadLine();
+
+                People.Add(contact);
+                Console.ForegroundColor= ConsoleColor.Green;
+                Console.WriteLine("Contact added successfully.");
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+        }
+        public void ViewContact()
+        {
+            foreach (var data in People)
+            {
+
+                Console.WriteLine("Name of the Person : " + data.FirstName + " " + data.LastName);
+                Console.WriteLine("Email ID : " + data.Email);
+                Console.WriteLine("Mobile Number : " + data.PhoneNumber);
+                Console.WriteLine("\n");
+
+            }
+
+
+        }
+    }
+    
+}
---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AddressBook;
+
+Console.WriteLine("Welcome to Address Book Program in AddressBookMain!");
+
+CreateContact contact = new CreateContact();
+
+while (true)
+{
+    Console.WriteLine("Enter the number :\n1)Create Contact\n2)View Contact");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            contact.AddContact();
+            break;
+        case 2:
+            contact.ViewContact();
+            break;
+    }
+}
